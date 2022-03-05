39e9c6b1b621027f662f44138ebe19b9e98c7375
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-03-05 18:25:30 +0700

    Update Program Jam Menit Detik.cpp

--- Program Jam Menit Detik.cpp
+++ Program Jam Menit Detik.cpp
@@ -1,28 +1,21 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-
-int main() {
-    
+int main()
+{
     int InputDetik;
     int Jam;
     int Menit;
     int Detik;
     
-    cin>>InputDetik;
+    cin >> InputDetik;
     
-    Jam=InputDetik/(60*60);
-    InputDetik=InputDetik-((60*60)*Jam);
-    Menit=InputDetik/60;
-    InputDetik=InputDetik-(60*Menit);
+    Jam = InputDetik/(60*60);
+    InputDetik = InputDetik-((60*60)*Jam);
+    Menit = InputDetik/60;
+    InputDetik = InputDetik-(60*Menit);
     
-    cout<<Jam<<" Jam "<<Menit<<" Menit "<<InputDetik<<" Detik ";
+    cout << Jam << " Jam " << Menit << " Menit " << InputDetik << " Detik ";
      
-    
     return 0;
 }
-
